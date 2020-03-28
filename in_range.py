212b7bd451980ef8355c5f5bc577135ba0237c0f
Owais Qayyum <qayum.insaane@gmail.com>
2020-03-28 21:31:17 +0100

    In Range Challenge

---
+++ in_range.py
@@ -0,0 +1,19 @@
+# In Range
+
+"""
+Create a function named in_range() that has three parameters named num, lower, and upper.
+The function should return True if num is greater than or equal to lower and less
+than or equal to upper. Otherwise, return False.
+"""
+
+
+def in_range(num, lower, upper):
+    if (num >= lower) and (num <= upper):
+        return True
+    else:
+        return False
+
+
+print(in_range(10, 10, 10))
+
+print(in_range(5, 10, 20))
